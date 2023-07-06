3a350d6dd2517c37c95fc1f437e06f2bc2fe65b0
Staroyee <danmasefield@gmail.com>
2023-07-06 21:35:26 +0930

    Added code to JS file (questions object variable and function to display questions).

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,3 +22,39 @@ var timeLeft = 75;
 var timerInterval = 0;
 var currentQuestion = 0;
 
+var questions = [
+    {
+    askQuestion: ["Commonly used data types DO NOT include:"],
+    choices: ["1. Strings", "2. Booleans", "3. Alerts", "4. Numbers"],
+    answer: "3. Alerts"
+},
+    {
+    askQuestion: ["The condition in an if/else statement is enclosed within ____."],
+    choices: ["1. Quotes", "2. Curly brackets", "3. Parentheses", "4. Square brackets"],
+    answer: "3. Parentheses"
+},
+    {
+    askQuestion: ["Arrays in JavaScript can be used to store ____."],
+    choices: ["1. Numbers and strings", "2. Other arrays", "3. Booleans", "4. All of the above"],
+    answer: "4. All of the above"
+},
+    {
+    askQuestion: ["String values must be enclosed within ____ when being assigned to variables."],
+    choices: ["1. Commas", "2. Curly brackets", "3. Quotes", "4. Parentheses"],
+    answer: "3. Quotes"
+},
+    {
+    askQuestion: ["A very useful tool used during development and debugging for printing content to the debugger is:"],
+    choices: ["1. JavaScript", "2. Terminal/bash", "3. for loops", "4. console.log"],
+    answer: "4. console.log"
+},
+];
+
+function displayQuestions(questionList) {
+    questionsEl.innerText = questionList.askQuestion[0];
+    choice1El.textContent = questionList.choices[0];
+    choice2El.textContent = questionList.choices[1];
+    choice3El.textContent = questionList.choices[2];
+    choice4El.textContent = questionList.choices[3];
+};
+
